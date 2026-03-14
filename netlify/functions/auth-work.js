cdbe30670dbfca62b77f40ec2b01acaea9efee47
kclose23 <kclose23@gmail.com>
2026-03-14 12:49:37 -0600

    Update auth-work.js

--- netlify/functions/auth-work.js
+++ netlify/functions/auth-work.js
@@ -2,8 +2,9 @@ exports.handler = async function() {
   const clientId = process.env.GOOGLE_WORK_CLIENT_ID;
   const redirectUri = process.env.GOOGLE_WORK_REDIRECT_URI;
   
-  const scope = encodeURIComponent(
-    'https://www.googleapis.com/auth/calendar.events'
+ const scope = encodeURIComponent(
+    'https://www.googleapis.com/auth/calendar.events https://www.googleapis.com/auth/gmail.send'
+  );
   );
   
   const authUrl = `https://accounts.google.com/o/oauth2/v2/auth?client_id=${clientId}&redirect_uri=${encodeURIComponent(redirectUri)}&response_type=code&scope=${scope}&access_type=offline&prompt=consent`;
