0cb0e42d66c65c7bda60a01ca8d991badb79c1cd
kclose23 <kclose23@gmail.com>
2026-03-14 11:53:18 -0600

    Create auth-family.js

---
+++ netlify/functions/auth-family.js
@@ -0,0 +1,15 @@
+exports.handler = async function() {
+  const clientId = process.env.GOOGLE_FAMILY_CLIENT_ID;
+  const redirectUri = process.env.GOOGLE_FAMILY_REDIRECT_URI;
+  
+  const scope = encodeURIComponent(
+    'https://www.googleapis.com/auth/calendar.events'
+  );
+  
+  const authUrl = `https://accounts.google.com/o/oauth2/v2/auth?client_id=${clientId}&redirect_uri=${encodeURIComponent(redirectUri)}&response_type=code&scope=${scope}&access_type=offline&prompt=consent`;
+  
+  return {
+    statusCode: 302,
+    headers: { Location: authUrl }
+  };
+};
