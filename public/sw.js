8209000c09da99d468824b36282be35bcacdbdda
kclose23 <kclose23@gmail.com>
2026-03-14 18:47:59 -0600

    Create sw.js

---
+++ public/sw.js
@@ -0,0 +1,18 @@
+var CACHE = 'aria-v1';
+var ASSETS = ['/', '/index.html', '/style.css', '/script.js'];
+
+self.addEventListener('install', function(e) {
+  e.waitUntil(
+    caches.open(CACHE).then(function(cache) {
+      return cache.addAll(ASSETS);
+    })
+  );
+});
+
+self.addEventListener('fetch', function(e) {
+  e.respondWith(
+    caches.match(e.request).then(function(cached) {
+      return cached || fetch(e.request);
+    })
+  );
+});
